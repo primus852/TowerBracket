8fbef7380d0f7025182f23215ff274ea98b266cd
Torte <tow.berlin@gmail.com>
2021-01-17 00:46:58 +0100

    Added Multiplayer

---
+++ Assets/Scripts/Multiplayer/MultiplayerMenu.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Multiplayer
+{
+    public class MultiplayerMenu : MonoBehaviourPunCallbacks
+    {
+        [Header("Screens")] public GameObject mainScreen;
+
+        public GameObject lobbyScreen;
+
+        [Header("Main Screen")] public Button createRoomButton;
+        public Button joinRoomButton;
+
+        [Header("Lobby Screen")] public Text playerListText;
+        public Button startGameButton;
+
+        private void Start()
+        {
+            createRoomButton.interactable = false;
+            joinRoomButton.interactable = false;
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            createRoomButton.interactable = true;
+            joinRoomButton.interactable = true;
+        }
+
+        private void SetScreen(GameObject screen)
+        {
+            mainScreen.SetActive(false);
+            lobbyScreen.SetActive(false);
+
+            screen.SetActive(true);
+        }
+
+        public void OnCreateRoomButton(TMP_InputField roomNameInput)
+        {
+            NetworkManager.networkManagerInstance.CreateRoom(roomNameInput.text);
+        }
+
+        public void OnJoinRoomButton(TMP_InputField roomNameInput)
+        {
+            NetworkManager.networkManagerInstance.JoinRoom(roomNameInput.text);
+        }
+
+        public void OnPlayerNameUpdate(TMP_InputField playerNameInput)
+        {
+            PhotonNetwork.NickName = playerNameInput.text;
+        }
+
+        public override void OnJoinedRoom()
+        {
+            SetScreen(lobbyScreen);
+
+            photonView.RPC("UpdateLobbyUI", RpcTarget.All);
+        }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            UpdateLobbyUI();
+        }
+
+        [PunRPC]
+        public void UpdateLobbyUI()
+        {
+            playerListText.text = "";
+
+            foreach (Player player in PhotonNetwork.PlayerList)
+            {
+                playerListText.text += player.NickName + "\n";
+            }
+
+            startGameButton.interactable = PhotonNetwork.IsMasterClient;
+        }
+
+        public void OnLeaveLobbyButton()
+        {
+            PhotonNetwork.LeaveRoom();
+            SetScreen(mainScreen);
+        }
+
+        public void OnStartGameButton()
+        {
+            NetworkManager.networkManagerInstance.photonView.RPC("ChangeScene", RpcTarget.All, "Level01" );
+        }
+    }
+}
---
+++ Assets/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public class NetworkManager : MonoBehaviourPunCallbacks
+    {
+        public static NetworkManager networkManagerInstance;
+
+        private void Awake()
+        {
+            if (networkManagerInstance != null && networkManagerInstance != this)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                networkManagerInstance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnCreatedRoom()
+        {
+            Debug.Log("Created Room: " + PhotonNetwork.CurrentRoom.Name);
+        }
+
+        public void CreateRoom(string roomName)
+        {
+            PhotonNetwork.CreateRoom(roomName);
+        }
+
+        public void JoinRoom(string roomName)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+
+        [PunRPC]
+        public void ChangeScene(string sceneName)
+        {
+            PhotonNetwork.LoadLevel(sceneName);
+        }
+    }
+}
